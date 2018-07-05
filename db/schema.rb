c05b72b2d80f6d229f233dbc4cd315858c83c1b6
abhilashsnruby <abhilashsnmca@gmail.com>
2018-07-05 18:46:05 +0530

    adds devise specific data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180705110612) do
+ActiveRecord::Schema.define(version: 20180705120734) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180705110612) do
     t.string "category_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
