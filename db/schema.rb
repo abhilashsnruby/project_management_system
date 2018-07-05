2c543e19d57f9b4244687c7437907a4a990f6023
abhilashsnruby <abhilashsnmca@gmail.com>
2018-07-05 17:24:05 +0530

    adds devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180705103707) do
+ActiveRecord::Schema.define(version: 20180705110612) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
