0e19a9cf5ac975851893d0f54f8a2712886521c0
abhilashsnruby <abhilashsnmca@gmail.com>
2018-07-09 21:03:19 +0530

    adds migration for comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180708022734) do
+ActiveRecord::Schema.define(version: 20180709153153) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment_text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
