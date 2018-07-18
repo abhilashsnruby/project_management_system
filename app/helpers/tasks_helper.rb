ceeae27148dad8e550aa0d09980cc06b4204cb42
abhilashsnruby <abhilashsnmca@gmail.com>
2018-07-18 15:49:25 +0530

    refactors hour and minute code

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -5,9 +5,16 @@ module TasksHelper
     created_date = (comment.created_at.strftime('%d').to_i)
     created_hour = comment.created_date_record
     current_hour = current_time.strftime('%H').to_i
-    hours_ago = current_hour - created_hour
-    hour = (created_hour == current_hour) ? "Just now created" : hours_ago.to_s
-    (created_hour < 24) ? hour : find_current_details(created_date)
+    @hours_ago = current_hour - created_hour
+    if (created_hour == current_hour)
+      @hour = "Just now created"
+    elsif ((created_hour > 1) && (created_hour < 24))
+      hours = Time.now.strftime('%H')
+      minutes = Time.now.strftime('%M')
+      @hour = (hours + " hour" + " and " + minutes + " minutes ago ")
+    elsif (created_hour > 24)
+      find_current_details(created_hour)
+    end
   end
 
   def find_current_details(created_date)
